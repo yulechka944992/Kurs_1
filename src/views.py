7214b0bc22d3a483c1ed9d0801c34ad9cb46e8c2
Юлия Кряжева <yulechka944992@gmail.com>
2026-04-29 21:59:59 +0900

    add new func

--- src/views.py
+++ src/views.py
@@ -1,16 +1,5 @@
-# def day_time_now():
-#     """
-#     Функция, которая приветствует в зависимости от текущего времени суток.
-#     Возвращает строку приветствия в зависимости от времени.
-#     """
-#     current_date_time = datetime.datetime.now()
-#     hour = current_date_time.hour
-#
-#     if 0 <= hour < 6 or 22 <= hour <= 23:
-#         return "Доброй ночи"
-#     elif 17 <= hour <= 22:
-#         return "Добрый вечер"
-#     elif 7 <= hour <= 11:
-#         return "Доброе утро"
-#     else:
-#         return "Добрый день"
+from utils import generate_analytics_data
+
+result = generate_analytics_data("2021-12-15 15:30:00")
+
+print(result)
